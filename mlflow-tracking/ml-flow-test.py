ee5abbf19d24e3cdc6c370c40ea53c1e40fa14a5
David <78443646+wittmajerd@users.noreply.github.com>
2026-04-19 12:18:45 +0200

    added new pipline to mlflow test

--- mlflow-tracking/ml-flow-test.py
+++ mlflow-tracking/ml-flow-test.py
@@ -1,19 +1,16 @@
 import mlflow
 import pandas as pd
-from sklearn.ensemble import HistGradientBoostingClassifier
-from process import (
-    prepare_raw_df, 
-    build_stratified_folds, 
-    apply_fairness_per_fold, 
-    onehot_encode_prepared_folds, 
-    MissingStrategy, 
-    EducationMode, 
-    FairnessMode, 
-    TARGET_COL,
-    SENSITIVE_COLS,
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from pipeline import (
+    MissingStrategy,
+    EducationMode,
+    FairnessMode,
+    training_pipeline,
+    evaluation_pipeline,
 )
 
-def ml_flow_train(X_train, y_train):
+def ml_flow_train(adult_df: pd.DataFrame, config: dict):
     mlflow.set_tracking_uri("http://host.docker.internal:5000") # <-- This line is if we are using the dev container
     # mlflow.set_tracking_uri("http://localhost:5000") # <-- This line is if we are running the script locally
     mlflow.set_experiment("Default")
@@ -21,34 +18,19 @@ def ml_flow_train(X_train, y_train):
     mlflow.sklearn.autolog()
 
     with mlflow.start_run():
-        gbr = HistGradientBoostingClassifier(random_state=42)
-        gbr.fit(X_train, y_train)
+        result, y_true = training_pipeline(adult_df, config=config)
+        # itt is lehetne logolni a resultot is mlflow-ba, de most csak az evaluation_pipeline-t hívjuk meg
+        evaluation_pipeline(y_true, result)
 
 if __name__ == "__main__":
     adult_df = pd.read_csv("data/adult.csv")
+    config = {
+        "missing_strategy": MissingStrategy.UNKNOWN,
+        "education_mode": EducationMode.NUM,
+        "fairness_mode": FairnessMode.NONE,
+        "target_col": "income",
+        "sensitive_cols": ["sex", "race"],
+        "model_type": GradientBoostingClassifier,  # sklearn classifier class, e.g. LogisticRegression, RandomForestClassifier, GradientBoostingClassifier
+    }
 
-    clean_df = prepare_raw_df(
-        adult_df,
-        missing_strategy=MissingStrategy.UNKNOWN,
-        education_mode=EducationMode.NUM,
-    )
-
-    folds = build_stratified_folds(clean_df, target_col=TARGET_COL, n_splits=5, random_state=42)
-    ready_folds = apply_fairness_per_fold(
-        folds,
-        target_col=TARGET_COL,
-        sensitive_cols=SENSITIVE_COLS,
-        fairness_mode=FairnessMode.NONE,
-    )
-    ready_folds = onehot_encode_prepared_folds(ready_folds)
-
-    print("Num folds:", len(ready_folds))
-    for fold_data in ready_folds:
-        print(
-            f"Fold {fold_data['fold']} - X_train shape: {fold_data['X_train'].shape}, "
-            f"X_test shape: {fold_data['X_test'].shape}, "
-            f"Train positive ratio: {fold_data['y_train'].mean().round(4)}"
-        )
-
-
-    ml_flow_train(ready_folds[0]['X_train'], ready_folds[0]['y_train'])
+    ml_flow_train(adult_df, config=config)
